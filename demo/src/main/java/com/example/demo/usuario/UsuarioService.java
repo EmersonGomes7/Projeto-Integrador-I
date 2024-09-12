5d88ce8beb466b83fa6b95f4860a155c09bdc536
Carlos Eduardo <caducouti7@gmail.com>
2024-09-12 11:01:41 -0300

    V7.1 TODO removido pois a sessão de código era desnecessária

--- demo/src/main/java/com/example/demo/usuario/UsuarioService.java
+++ demo/src/main/java/com/example/demo/usuario/UsuarioService.java
@@ -1,9 +1,5 @@
 package com.example.demo.usuario;
 
-import com.example.demo.frequencia.FrequenciaRepository;
-import com.example.demo.producao.ProducaoRepository;
-import com.example.demo.publicacao.PublicacaoRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -33,31 +29,4 @@ public class UsuarioService {
         }
     }
 
-    @Autowired
-    private ProducaoRepository producaoRepository;
-
-    @Autowired
-    private PublicacaoRepository publicacaoRepository;
-
-    @Autowired
-    private FrequenciaRepository frequenciaRepository;
-
-    @Autowired
-    private UsuarioRepository usuarioRepository;
-
-    /* TODO: VER SE ISSO É USUAL
-    @Transactional
-    public void deleteProducoesPublicacoesFrequencias(Long usuarioId) {
-        Usuario usuario = usuarioRepository.findById(usuarioId).orElseThrow(() -> new EntityNotFoundException("Usuario não encontrado"));
-
-        // Deleta as produções associadas ao usuário
-        producaoRepository.deleteByIdUsuarioCriador(usuario);
-
-        // Deleta as publicações associadas ao usuário
-        publicacaoRepository.deleteByIdUsuarioCriador(usuario);
-
-        // Deleta as frequências associadas ao usuário
-        frequenciaRepository.deleteByIdUsuarioProf(usuario);
-    }
-    */
 }
